9d68b00533d3efeb6550ccd3f25060c1d215241a
Heitor Campagnac <156446279+Heitor1805@users.noreply.github.com>
2025-01-29 18:56:08 -0300

    rock,paper,scissor.py

--- pedra,papel,tesoura.py
+++ pedra,papel,tesoura.py
@@ -1,21 +1,21 @@
 import random
-x=input("pedra, papel or tesoura?: ").lower()
-options=["pedra","papel","tesoura"]
+x=input("rock, paper or scissor?: ").lower()
+options=["rock","paper","scissor"]
 maquina=random.choice(options)
 if x==maquina:
     print("Draw")
-elif x=="pedra" and maquina=="tesoura":
-    print("Você venceu")
-elif x=="pedra" and maquina=="papel":
-    print("Você perdeu")
-elif x=="papel" and maquina=="pedra":
-    print("PLayer won")
-elif x=="papel" and maquina=="tesoura":
-    print("Você perdeu")
-elif x=="tesoura" and maquina=="pedra":
-    print("Você perdeu")
-elif x=="tesoura" and maquina=="papel":
-    print("Você venceu")
+elif x=="rock" and maquina=="scissor":
+    print("Player won")
+elif x=="rock" and maquina=="paper":
+    print("Player lose")
+elif x=="paper" and maquina=="rock":
+    print("Player won")
+elif x=="paper" and maquina=="scissor":
+    print("Player lose")
+elif x=="scissor" and maquina=="rock":
+    print("Player lose")
+elif x=="scissor" and maquina=="paper":
+    print("Player won")
     
 
 
